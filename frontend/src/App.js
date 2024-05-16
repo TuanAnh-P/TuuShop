4b9c687d8ee85bd3a8f9dfb2d68825a42041e75f
TuanAnh-P <1tuananhp@gmail.com>
2024-05-16 12:41:08 -0400

    Starting to set up backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return (
-    <>
-      <Header/>
-      <main>
-        <Container>
-          <h1>Welcome to TuuShop</h1>
-        </Container>
-      </main>
-      <Footer/>
-    </>
-  )
-}
+	return (
+		<>
+			<Header />
+			<main>
+				<Container>
+					<Outlet />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
 
-export default App
+export default App;
