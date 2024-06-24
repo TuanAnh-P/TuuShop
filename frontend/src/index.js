33a7d76a8b6490645a18e94e005be63010ee8841
TuanAnh-P <1tuananhp@gmail.com>
2024-06-24 19:00:11 -0400

    Shopping Cart Functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,23 +15,38 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' element={<App />}>
-			<Route index={true} path='/' element={<HomeScreen />} />
-			<Route path='product/:id' element={<ProductScreen />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<App />}
+    >
+      <Route
+        index={true}
+        path='/'
+        element={<HomeScreen />}
+      />
+      <Route
+        path='product/:id'
+        element={<ProductScreen />}
+      />
+      <Route
+        path='/cart'
+        element={<CartScreen />}
+      />
+    </Route>
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
