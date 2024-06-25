9da68b5b81bcde7920017ab38ebf5bdb20bb0c47
TuanAnh-P <1tuananhp@gmail.com>
2024-06-25 15:53:56 -0400

    Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'; // Import cors
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -16,11 +18,19 @@ const app = express();
 // Enable CORS
 app.use(cors());
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-	res.send('API is running ...');
+  res.send('API is running ...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
