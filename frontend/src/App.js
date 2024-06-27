0a5b0ffa57351d179144137e82ed6f4e46850129
TuanAnh-P <1tuananhp@gmail.com>
2024-06-27 15:16:14 -0400

    Checkpoint 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
-	return (
-		<>
-			<Header />
-			<main>
-				<Container>
-					<Outlet />
-				</Container>
-			</main>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
