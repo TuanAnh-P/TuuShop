165ecd474eaeddea5e9c994ba8e21005e0c70b67
TuanAnh-P <1tuananhp@gmail.com>
2024-06-28 11:51:13 -0400

    Frontend Authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -17,41 +17,27 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path='/'
-      element={<App />}
-    >
-      <Route
-        index={true}
-        path='/'
-        element={<HomeScreen />}
-      />
-      <Route
-        path='product/:id'
-        element={<ProductScreen />}
-      />
-      <Route
-        path='/cart'
-        element={<CartScreen />}
-      />
-      <Route
-        path='/login'
-        element={<LoginScreen />}
-      />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<HomeScreen />} />
+			<Route path='product/:id' element={<ProductScreen />} />
+			<Route path='/cart' element={<CartScreen />} />
+			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
+		</Route>
+	)
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
 );
 
 reportWebVitals();
